c414b5cf68aba95aa0522cac160790adf0edbe45
eschindev <118208710+eschindev@users.noreply.github.com>
2023-04-07 00:18:40 -0700

    added comments, removed console.logs, removed commented-out HTML that is now auto-generated

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var citySearchBox = $('#city-search-box');
 var currentDayOutput = $('#current-day-output');
 var fiveDayHeader = $('#five-day-header');
 var fiveDayForecast = $('#five-day-forecast');
-var searchHistory = $('#search-history')
+var searchHistory = $('#search-history');
 
 
 
@@ -18,8 +18,6 @@ function getCoords(city) {
             return response.json();
         })
         .then(function(data) {
-            console.log("getCoords");
-            console.log(data);
             currentDayOutput.empty();
             fiveDayForecast.empty();
             buttonsFromLS();
@@ -36,14 +34,12 @@ function getCurrent(lat, lon) {
             return response.json();
         })
         .then(function(data) {
-            console.log("getCurrent")
-            console.log(data);
-
-            currentDayOutput.addClass("current-day-class")
+            // generate the card for current day
+            currentDayOutput.addClass("current-day-class");
             var currentDayHeader = $("<h2>").text(`${data.name} on ${dayjs().format("M/D/YYYY")}`);
             currentDayOutput.append(currentDayHeader);
             var iconCode = data.weather[0].icon;
-            var icon = $("<img>").attr("src", `https://openweathermap.org/img/w/${iconCode}.png`)
+            var icon = $("<img>").attr("src", `https://openweathermap.org/img/w/${iconCode}.png`);
             currentDayOutput.append(icon);
             var currentTemp = $("<p>").html(`Temp: ${data.main.temp}&deg;F`);
             var currentWind = $("<p>").text(`Wind: ${data.wind.speed}mph`);
@@ -60,14 +56,11 @@ function getFiveDay(lat, lon) {
             return response.json();
         })
         .then(function(data) {
-            console.log("getFiveDay");
-            console.log(data);
-
             //generate 5-day forecast header
             var fdh = $("<h2>").text("5-Day Forecast:")
             fiveDayHeader.html(fdh);
+            // generate each card for the 5-day forecast and append to forecastBox
             for(i = 3; i < data.list.length; i += 8) {
-                console.log(data.list[i].dt);
                 var forecastBox = $("<div>").addClass("forecast-box col-2");
                 var forecastHeader = $("<h4>").addClass("forecast-header").text(dayjs.unix(data.list[i].dt).format("M/D/YY"))
                 forecastBox.append(forecastHeader);
@@ -83,7 +76,7 @@ function getFiveDay(lat, lon) {
             
         })
 }
-
+// function that receives the event and handles checking localStorage, and determines how to proceed based on event target
 function searchCity(event) {
     event.preventDefault();
     if (event.target.id === "city-search-btn"){
@@ -93,23 +86,24 @@ function searchCity(event) {
             return;
         }
         if (!localStorage.getItem(city)) {
-            localStorage.setItem(city, city);
+            localStorage.setItem(city, "weather-planner");
         }
     } else {
         var city = event.target.textContent;
     }
-
     getCoords(city);
 }
-
+// Creates buttons for search history from localStorage, checking to make sure they originated from this application using value to store that info
 function buttonsFromLS() {
     searchHistory.empty();
     for (i = 0; i < localStorage.length; i++) {
-        var histButton = $("<button>").addClass("custom-btn my-1").text(localStorage.key(i));
-        searchHistory.append(histButton);
+        if (localStorage.getItem(localStorage.key(i)) === "weather-planner"){
+            var histButton = $("<button>").addClass("custom-btn my-1").text(localStorage.key(i));
+            searchHistory.append(histButton);
+        }
     }
 }
-
+// render buttons on page load
 buttonsFromLS();
-
+// listen for submit on form, or click on either form or history buttons
 searchArea.on("click submit", ".custom-btn", searchCity);
